7602a3ed8de83c95f5dc176d4fa5e9c40d728808
zhangjunfei <510446914@qq.com>
2020-04-30 17:17:48 +0800

    添webview类

--- DeveloperHelper/developerandroidx/src/main/java/com/example/developerandroidx/ui/webView/WebViewActivity.java
+++ DeveloperHelper/developerandroidx/src/main/java/com/example/developerandroidx/ui/webView/WebViewActivity.java
@@ -4,7 +4,7 @@ import com.example.developerandroidx.R;
 import com.example.developerandroidx.base.BaseActivity;
 
 /**
- * 参考：https://www.jianshu.com/p/3e0136c9e748
+ * 参考：https://www.jianshu.com/p/3e0136c9e748 https://www.jianshu.com/p/5cc2eae14e07
  */
 public class WebViewActivity extends BaseActivity {
 
