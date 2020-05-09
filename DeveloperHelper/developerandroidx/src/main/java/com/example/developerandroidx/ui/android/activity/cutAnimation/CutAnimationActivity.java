fc2c02b758eba433808fe2db90a8b730a998d608
zhangjunfei <510446914@qq.com>
2020-05-09 20:40:04 +0800

    增加activity切换动画

---
+++ DeveloperHelper/developerandroidx/src/main/java/com/example/developerandroidx/ui/android/activity/cutAnimation/CutAnimationActivity.java
@@ -0,0 +1,12 @@
+package com.example.developerandroidx.ui.android.activity.cutAnimation;
+
+import com.example.developerandroidx.R;
+import com.example.developerandroidx.base.BaseActivity;
+
+public class CutAnimationActivity extends BaseActivity {
+    
+    @Override
+    protected int bindLayout() {
+        return R.layout.activity_cut_animation;
+    }
+}
