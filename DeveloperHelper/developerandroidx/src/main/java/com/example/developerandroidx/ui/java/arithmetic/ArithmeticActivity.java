717d0d881ac51a4048e98c1b74293efc9d485731
zhangjunfei <510446914@qq.com>
2020-05-12 17:08:51 +0800

    冒泡排序

---
+++ DeveloperHelper/developerandroidx/src/main/java/com/example/developerandroidx/ui/java/arithmetic/ArithmeticActivity.java
@@ -0,0 +1,68 @@
+package com.example.developerandroidx.ui.java.arithmetic;
+
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.developerandroidx.R;
+import com.example.developerandroidx.base.BaseActivity;
+import com.example.developerandroidx.utils.AnimUtil;
+import com.example.developerandroidx.view.navigationView.utils.PixelTransformUtil;
+import com.kongzue.dialog.v3.FullScreenDialog;
+
+import butterknife.OnClick;
+
+public class ArithmeticActivity extends BaseActivity {
+
+    @Override
+    protected int bindLayout() {
+        return R.layout.activity_arithmetic;
+    }
+
+    @Override
+    protected void initView() {
+        setTitle("算法");
+    }
+
+    @OnClick({R.id.btn_bubble_sort})
+    public void click(View v) {
+        switch (v.getId()) {
+            case R.id.btn_bubble_sort:
+                FullScreenDialog.show((AppCompatActivity) context, R.layout.view_arithmetic_dialog, new FullScreenDialog.OnBindView() {
+                    @Override
+                    public void onBind(FullScreenDialog dialog, View rootView) {
+                        bubbleSort(rootView);
+                    }
+                });
+                break;
+        }
+    }
+
+    /**
+     * 冒泡排序
+     *
+     * @param rootView
+     */
+    private void bubbleSort(View rootView) {
+        TextView point;
+        LinearLayout.LayoutParams params;
+        LinearLayout ll_content = rootView.findViewById(R.id.ll_content);
+        for (int i = 0; i < 6; i++) {
+            point = new TextView(context);
+            params = new LinearLayout.LayoutParams(PixelTransformUtil.dip2px(context, 35), PixelTransformUtil.dip2px(context, 35));
+            int margin = PixelTransformUtil.dip2px(context, 5);
+            params.setMargins(margin, margin, margin, margin);
+            point.setLayoutParams(params);
+            point.setBackgroundResource(R.drawable.bg_circle_main_color);
+            point.setTextColor(getResources().getColor(R.color.white));
+            point.setTextSize(14);
+            point.setText("1");
+            point.setGravity(Gravity.CENTER);
+            point.startAnimation(AnimUtil.getInstance().getScaleAnim(500, 500 + i * 100));
+            ll_content.addView(point);
+        }
+    }
+}
---
+++ DeveloperHelper/developerandroidx/src/main/java/com/example/developerandroidx/utils/AnimUtil.java
@@ -0,0 +1,63 @@
+package com.example.developerandroidx.utils;
+
+import android.view.animation.Animation;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
+
+/**
+ * @作者： zjf 2020/5/12 16:42
+ * @参考：
+ * @描述： 动画
+ */
+public class AnimUtil {
+
+    private static AnimUtil animUtil;
+    private Animation animation;
+
+    private AnimUtil() {
+    }
+
+    public static AnimUtil getInstance() {
+        if (animUtil == null) {
+            animUtil = new AnimUtil();
+        }
+
+        return animUtil;
+    }
+
+    /**
+     * 缩放动画
+     *
+     * @param duration
+     * @param startOffset
+     * @return
+     */
+    public Animation getScaleAnim(int duration, int startOffset) {
+        /**
+         *
+         * @param fromX 起始x轴位置，0为最小，1为原始，float形
+         * @param toX 同上
+         * @param fromY 同上T
+         * @param toY 同上
+         * @param pivotXType 用来约束pivotXValue的取值。取值有三种：Animation.ABSOLUTE，Animation.RELATIVE_TO_SELF，Animation.RELATIVE_TO_PARENT
+         * Type：Animation.ABSOLUTE：绝对，如果设置这种类型，后面pivotXValue取值就必须是像素点；比如：控件X方向上的中心点，pivotXValue的取值mIvScale.getWidth() / 2f
+         *      Animation.RELATIVE_TO_SELF：相对于控件自己，设置这种类型，后面pivotXValue取值就会去拿这个取值是乘上控件本身的宽度；比如：控件X方向上的中心点，pivotXValue的取值0.5f
+         *      Animation.RELATIVE_TO_PARENT：相对于它父容器（这个父容器是指包括这个这个做动画控件的外一层控件）， 原理同上，
+         * @param pivotXValue  配合pivotXType使用，原理在上面
+         * @param pivotYType 同from/to
+         * @param pivotYValue 原理同上
+         */
+        animation = new ScaleAnimation(0.0f, 1.0f, 0.0f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, 1, 0.5f);
+        animation.setDuration(duration);
+        animation.setStartOffset(startOffset);
+        return animation;
+    }
+
+    public Animation getTranslateAnim(float fromXDelta, float toXDelta, float fromYDelta, float toYDelta, int duration, int startOffset) {
+        animation = new TranslateAnimation(fromXDelta, toXDelta, fromYDelta, toYDelta);
+        animation.setDuration(duration);
+        animation.setStartOffset(startOffset);
+        return animation;
+    }
+}
