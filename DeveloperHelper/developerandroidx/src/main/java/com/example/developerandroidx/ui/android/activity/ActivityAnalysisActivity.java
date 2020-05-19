cb04b2a36ee708ef27d06e6f1b10848d3a690d66
zhangjunfei <510446914@qq.com>
2020-05-19 21:07:22 +0800

    代码优化

--- DeveloperHelper/developerandroidx/src/main/java/com/example/developerandroidx/ui/android/activity/ActivityAnalysisActivity.java
+++ DeveloperHelper/developerandroidx/src/main/java/com/example/developerandroidx/ui/android/activity/ActivityAnalysisActivity.java
@@ -12,7 +12,6 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -25,14 +24,11 @@ import com.example.developerandroidx.ui.android.activity.launchMode.SingleTaskAc
 import com.example.developerandroidx.ui.android.activity.launchMode.SingleTopActivity;
 import com.example.developerandroidx.ui.android.activity.launchMode.StandardActivity;
 import com.example.developerandroidx.ui.android.activity.lifeCycle.DialogTestctivity;
+import com.example.developerandroidx.ui.android.activity.transitionAnimation.ActivityAnalysisDescDialog;
 import com.example.developerandroidx.ui.android.activity.transitionAnimation.TransitionAnimationActivity;
 import com.example.developerandroidx.utils.CodeVariate;
 import com.example.developerandroidx.utils.DialogUtils;
 import com.example.developerandroidx.utils.RouteUtil;
-import com.example.developerandroidx.view.ExtensibleScrollView.ExtensibleScrollView;
-import com.kongzue.dialog.interfaces.OnMenuItemClickListener;
-import com.kongzue.dialog.v3.BottomMenu;
-import com.kongzue.dialog.v3.FullScreenDialog;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -148,45 +144,8 @@ public class ActivityAnalysisActivity extends BaseActivity {
                     }
                 });
                 break;
-            case R.id.iv_right:
-                DialogUtils.getInstance().shouFullScreenDialog(context, R.layout.dialog_activity_lifecycle, new DialogUtils.OnFullScreenDialogBindView() {
-                    @Override
-                    public void onBind(FullScreenDialog dialog, View rootView) {
-                        ExtensibleScrollView esv_content = rootView.findViewById(R.id.esv_content);
-//                        esv_content.addText("",ExtensibleScrollView.InsertTextType.TITLE_2,R.color.textColorBlack);
-                        esv_content.addText("Activity的概念", ExtensibleScrollView.InsertTextType.TITLE_2, R.color.textColorBlack);
-                        esv_content.addText("Activity 类是 Android 应用的关键组件，而 Activity 的启动和组合方式则是该平台应用模型的基本组成部分。在编程范式中，应用是通过 main() 方法启动的，而 Android 系统与此不同，它会调用与其生命周期特定阶段相对应的特定回调方法来启动 Activity 实例中的代码。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
-                        esv_content.addText("Activity 提供窗口供应用在其中绘制界面。此窗口通常会填满屏幕，但也可能比屏幕小，并浮动在其他窗口上面。通常，一个 Activity 实现应用中的一个屏幕。例如，应用中的一个 Activity 实现“偏好设置”屏幕，而另一个 Activity 实现“选择照片”屏幕。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
-                        esv_content.addText("Activity的生命周期", ExtensibleScrollView.InsertTextType.TITLE_2, R.color.textColorBlack);
-                        esv_content.addImage(R.mipmap.activity_lifecycle, 400);
-                        esv_content.addText("", ExtensibleScrollView.InsertTextType.TITLE_2, R.color.textColorBlack);
-
-                        esv_content.addText("onCreate()", ExtensibleScrollView.InsertTextType.TITLE_2, R.color.textColorBlack);
-                        esv_content.addText("您必须实现此回调，它会在系统创建您的 Activity 时触发。您的实现应该初始化 Activity 的基本组件：例如，您的应用应该在此处创建视图并将数据绑定到列表。最重要的是，您必须在此处调用 setContentView() 来定义 Activity 界面的布局。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
-
-                        esv_content.addText("onStart()", ExtensibleScrollView.InsertTextType.TITLE_2, R.color.textColorBlack);
-                        esv_content.addText("onCreate() 退出后，Activity 将进入“已启动”状态，并对用户可见。此回调包含 Activity 进入前台与用户进行互动之前的最后准备工作。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
-
-                        esv_content.addText("onResume()", ExtensibleScrollView.InsertTextType.TITLE_2, R.color.textColorBlack);
-                        esv_content.addText("系统会在 Activity 开始与用户互动之前调用此回调。此时，该 Activity 位于 Activity 堆栈的顶部，并会捕获所有用户输入。应用的大部分核心功能都是在 onResume() 方法中实现的。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
-
-                        esv_content.addText("onPause()", ExtensibleScrollView.InsertTextType.TITLE_2, R.color.textColorBlack);
-                        esv_content.addText("当 Activity 失去焦点并进入“已暂停”状态时，系统就会调用 onPause()。例如，当用户点按“返回”或“最近使用的应用”按钮时，就会出现此状态。当系统为您的 Activity 调用 onPause() 时，从技术上来说，这意味着您的 Activity 仍然部分可见，但大多数情况下，这表明用户正在离开该 Activity，该 Activity 很快将进入“已停止”或“已恢复”状态。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
-                        esv_content.addText("如果用户希望界面继续更新，则处于“已暂停”状态的 Activity 也可以继续更新界面。例如，显示导航地图屏幕或播放媒体播放器的 Activity 就属于此类 Activity。即使此类 Activity 失去了焦点，用户仍希望其界面继续更新。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
-                        esv_content.addText("您不应使用 onPause() 来保存应用或用户数据、进行网络呼叫或执行数据库事务。有关保存数据的信息，请参阅保存和恢复 Activity 状态。onPause() 执行完毕后，下一个回调为 onStop()或 onResume()，具体取决于 Activity 进入“已暂停”状态后发生的情况", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
-
-                        esv_content.addText("onStop()", ExtensibleScrollView.InsertTextType.TITLE_2, R.color.textColorBlack);
-                        esv_content.addText("当 Activity 对用户不再可见时，系统会调用 onStop()。出现这种情况的原因可能是 Activity 被销毁，新的 Activity 启动，或者现有的 Activity 正在进入“已恢复”状态并覆盖了已停止的 Activity。在所有这些情况下，停止的 Activity 都将完全不再可见。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
-                        esv_content.addText("系统调用的下一个回调将是 onRestart()（如果 Activity 重新与用户互动）或者 onDestroy()（如果 Activity 彻底终止）。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
-
-                        esv_content.addText("onRestart()", ExtensibleScrollView.InsertTextType.TITLE_2, R.color.textColorBlack);
-                        esv_content.addText("当处于“已停止”状态的 Activity 即将重启时，系统就会调用此回调。onRestart() 会从 Activity 停止时的状态恢复 Activity。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
-                        esv_content.addText("此回调后面总是跟着 onStart()。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
-
-                        esv_content.addText("onDestroy()", ExtensibleScrollView.InsertTextType.TITLE_2, R.color.textColorBlack);
-                        esv_content.addText("系统会在销毁 Activity 之前调用此回调。此回调是 Activity 接收的最后一个回调。通常，实现 onDestroy() 是为了确保在销毁 Activity 或包含该 Activity 的进程时释放该 Activity 的所有资源。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
-                    }
-                });
+            case R.id.iv_right://activity描述弹框
+                new ActivityAnalysisDescDialog().show(context);
                 break;
         }
     }
---
+++ DeveloperHelper/developerandroidx/src/main/java/com/example/developerandroidx/ui/android/service/ServiceDescDialog.java
@@ -0,0 +1,58 @@
+package com.example.developerandroidx.ui.android.service;
+
+import android.content.Context;
+import android.view.View;
+
+import com.example.developerandroidx.R;
+import com.example.developerandroidx.utils.DialogUtils;
+import com.example.developerandroidx.view.ExtensibleScrollView.ExtensibleScrollView;
+import com.kongzue.dialog.v3.FullScreenDialog;
+
+/**
+ * Date: 2020/5/19 20:28
+ * 参考:
+ * 描述: service描述弹框
+ */
+public class ServiceDescDialog {
+
+    public void show(Context context) {
+        DialogUtils.getInstance().shouFullScreenDialog(context, R.layout.dialog_service_desc, new DialogUtils.OnFullScreenDialogBindView() {
+            @Override
+            public void onBind(FullScreenDialog dialog, View rootView) {
+                ExtensibleScrollView esv_content = rootView.findViewById(R.id.esv_content);
+                esv_content.addText("Service 是一种可在后台执行长时间运行操作而不提供界面的应用组件。服务可由其他应用组件启动，而且即使用户切换到其他应用，服务仍将在后台继续运行。此外，组件可通过绑定到服务与之进行交互，甚至是执行进程间通信 (IPC)。例如，服务可在后台处理网络事务、播放音乐，执行文件 I/O 或与内容提供程序进行交互。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
+                esv_content.addImage(R.mipmap.service_lifecycle, 350);
+                esv_content.addText("前台服务", ExtensibleScrollView.InsertTextType.TITLE_2, R.color.textColorBlack);
+                esv_content.addText("前台服务执行一些用户能注意到的操作。例如，音频应用会使用前台服务来播放音频曲目。前台服务必须显示通知。即使用户停止与应用的交互，前台服务仍会继续运行。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
+                esv_content.addText("后台服务", ExtensibleScrollView.InsertTextType.TITLE_2, R.color.textColorBlack);
+                esv_content.addText("后台服务执行用户不会直接注意到的操作。例如，如果应用使用某个服务来压缩其存储空间，则此服务通常是后台服务。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
+                esv_content.addText("绑定服务", ExtensibleScrollView.InsertTextType.TITLE_2, R.color.textColorBlack);
+                esv_content.addText("当应用组件通过调用 bindService() 绑定到服务时，服务即处于绑定状态。绑定服务会提供客户端-服务器接口，以便组件与服务进行交互、发送请求、接收结果，甚至是利用进程间通信 (IPC) 跨进程执行这些操作。仅当与另一个应用组件绑定时，绑定服务才会运行。多个组件可同时绑定到该服务，但全部取消绑定后，该服务即会被销毁。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
+                esv_content.addText("注意事项", ExtensibleScrollView.InsertTextType.TITLE_2, R.color.colorRed);
+                esv_content.addText("虽然本文档分开概括讨论启动服务和绑定服务，但您的服务可同时以这两种方式运行，换言之，它既可以是启动服务（以无限期运行），亦支持绑定。唯一的问题在于您是否实现一组回调方法：onStartCommand()（让组件启动服务）和 onBind()（实现服务绑定）。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
+                esv_content.addText("无论服务是处于启动状态还是绑定状态（或同时处于这两种状态），任何应用组件均可像使用 Activity 那样，通过调用 Intent 来使用服务（即使此服务来自另一应用）。不过，您可以通过清单文件将服务声明为私有服务，并阻止其他应用访问该服务。使用清单文件声明服务部分将对此做更详尽的阐述。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
+                esv_content.addText("服务在其托管进程的主线程中运行，它既不创建自己的线程，也不在单独的进程中运行（除非另行指定）。如果服务将执行任何 CPU 密集型工作或阻止性操作（例如 MP3 播放或联网），则应通过在服务内创建新线程来完成这项工作。通过使用单独的线程，您可以降低发生“应用无响应”(ANR) 错误的风险，而应用的主线程仍可继续专注于运行用户与 Activity 之间的交互。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
+                esv_content.addText("在服务和线程之间进行选择", ExtensibleScrollView.InsertTextType.TITLE_2, R.color.textColorBlack);
+                esv_content.addText("简单地说，服务是一种即使用户未与应用交互也可在后台运行的组件，因此，只有在需要服务时才应创建服务。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
+                esv_content.addText("如果您必须在主线程之外执行操作，但只在用户与您的应用交互时执行此操作，则应创建新线程。例如，如果您只是想在 Activity 运行的同时播放一些音乐，则可在 onCreate() 中创建线程，在 onStart() 中启动线程运行，然后在 onStop() 中停止线程。您还可考虑使用 AsyncTask 或 HandlerThread，而非传统的 Thread 类。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
+                esv_content.addText("请记住，如果您确实要使用服务，则默认情况下，它仍会在应用的主线程中运行，因此，如果服务执行的是密集型或阻止性操作，则您仍应在服务内创建新线程。", ExtensibleScrollView.InsertTextType.BODY, R.color.colorRed);
+                esv_content.addText("绑定到已启动服务", ExtensibleScrollView.InsertTextType.TITLE_2, R.color.textColorBlack);
+                esv_content.addText("您可以创建同时具有已启动和已绑定两种状态的服务。换言之，可通过调用 startService() 启动服务，让服务无限期运行；此外，还可通过调用 bindService() 让客户端绑定到该服务。如果您确实允许服务同时具有已启动和已绑定状态，则在启动服务后，如果所有客户端均解绑服务，则系统不会销毁该服务。为此，您必须通过调用 stopSelf() 或 stopService() 显式停止服务。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
+                esv_content.addText("管理绑定服务的生命周期", ExtensibleScrollView.InsertTextType.TITLE_2, R.color.textColorBlack);
+                esv_content.addText("当取消服务与所有客户端之间的绑定时，Android 系统会销毁该服务（除非您还使用 onStartCommand() 启动了该服务）。因此，如果您的服务完全是绑定服务，则您无需管理其生命周期，Android 系统会根据它是否绑定到任何客户端代您管理。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
+                esv_content.addText("不过，如果您选择实现 onStartCommand() 回调方法，则您必须显式停止服务，因为系统现已将其视为已启动状态。在此情况下，服务将一直运行，直到其通过 stopSelf() 自行停止，或其他组件调用 stopService()（与该服务是否绑定到任何客户端无关）。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
+                esv_content.addText("此外，如果您的服务已启动并接受绑定，则当系统调用您的 onUnbind() 方法时，如果您想在客户端下一次绑定到服务时接收 onRebind() 调用，则可选择返回 true。onRebind() 返回空值，但客户端仍在其 onServiceConnected() 回调中接收 IBinder。下图说明这种生命周期的逻辑。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
+                esv_content.addImage(R.mipmap.service_binding_tree_lifecycle, 350);
+
+                esv_content.addText("IntentService", ExtensibleScrollView.InsertTextType.TITLE_2, R.color.textColorBlack);
+                esv_content.addText("IntentService 类为在单个后台线程上运行操作提供了一个简单明了的结构。这使它能够在不影响界面响应速度的情况下处理长时间运行的操作。此外，IntentService 不受大多数界面生命周期事件的影响，因此它能够在会关闭 AsyncTask 的情况下继续运行", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
+                esv_content.addText("IntentService 有一些限制：", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
+                esv_content.addText("1.它无法直接与您的界面互动。要在界面中显示其结果，您必须将结果发送到 Activity。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
+                esv_content.addText("2.工作请求依序运行。如果某个操作在 IntentService 中运行，并且您向其发送了另一个请求，则该请求会等待第一个操作完成。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
+                esv_content.addText("3.在 IntentService 上运行的操作无法中断。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
+                esv_content.addText("但在大多数情况下，执行简单后台操作的首选方式是 IntentService。", ExtensibleScrollView.InsertTextType.BODY, R.color.textColor);
+
+            }
+        });
+    }
+}
